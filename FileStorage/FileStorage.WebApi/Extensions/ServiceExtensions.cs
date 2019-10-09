2d9bf6a57eec430f82866ad7169a3548323cd534
v.kovaliov <v.kovaliov@polytech.software>
2019-10-09 12:40:21 +0300

    Add ServiceExtensions

---
+++ FileStorage/FileStorage.WebApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,104 @@
+﻿// <copyright file="ServiceExtensions.cs" company="Kovalov Systems">
+// Confidential and Proprietary
+// Copyright 2019 Kovalov Systems
+// ALL RIGHTS RESERVED.
+// </copyright>
+
+using AutoMapper;
+using FileStorage.Implementation.AutoMapperConfig;
+using FileStorage.Implementation.DataAccess;
+using FileStorage.Implementation.DataAccess.Entities;
+using FileStorage.WebApi.Options;
+using FileStorage.WebApi.Swagger;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace FileStorage.WebApi.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureAutoMapper(this IServiceCollection services)
+        {
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            var mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+        }
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<UserEntity, IdentityRole>(options =>
+                    {
+                        options.User.RequireUniqueEmail = true;
+                    })
+                    .AddEntityFrameworkStores<StorageContext>()
+                    .AddDefaultTokenProviders();
+        }
+
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "FileStorage API",
+                    Version = "v1"
+                });
+
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "Standard Authorization header using the Bearer scheme. Example: \"bearer {token}\"",
+                    In = "header",
+                    Name = "Authorization",
+                    Type = "apiKey"
+                });
+
+                c.DocumentFilter<SecurityRequirementsDocumentFilter>();
+            });
+        }
+
+        public static void ConfigureAuthorization(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtAuthenticationSection = configuration.GetSection("JwtAuthentication");
+            services.Configure<JwtAuthenticationOptions>(jwtAuthenticationSection);
+            var jwtAuthenticationOptions = jwtAuthenticationSection.Get<JwtAuthenticationOptions>();
+
+            services.AddAuthentication(x =>
+                    {
+                        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    })
+                    .AddJwtBearer(x =>
+                    {
+                        x.RequireHttpsMetadata = false;
+                        x.SaveToken = true;
+                        x.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = jwtAuthenticationOptions.SymmetricSecurityKey,
+                            ValidateIssuer = false,
+                            ValidateAudience = false
+                        };
+                    });
+        }
+
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                                  builder => builder.AllowAnyOrigin()
+                                                    .AllowAnyMethod()
+                                                    .AllowAnyHeader()
+                                                    .AllowCredentials());
+            });
+        }
+    }
+}
--- FileStorage/FileStorage.WebApi/Startup.cs
+++ FileStorage/FileStorage.WebApi/Startup.cs
@@ -4,8 +4,6 @@
 // ALL RIGHTS RESERVED.
 // </copyright>
 
-using AutoMapper;
-using FileStorage.Implementation.AutoMapperConfig;
 using FileStorage.Implementation.DataAccess;
 using FileStorage.Implementation.DataAccess.Entities;
 using FileStorage.Implementation.DataAccess.Repositories;
@@ -13,9 +11,7 @@ using FileStorage.Implementation.DataAccess.RepositoryInterfaces;
 using FileStorage.Implementation.Interfaces;
 using FileStorage.Implementation.Options;
 using FileStorage.Implementation.Services;
-using FileStorage.WebApi.Options;
-using FileStorage.WebApi.Swagger;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using FileStorage.WebApi.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -25,8 +21,6 @@ using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-using Swashbuckle.AspNetCore.Swagger;
 using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
 
 namespace FileStorage.WebApi
@@ -49,12 +43,8 @@ namespace FileStorage.WebApi
                 options.UseSqlite(Configuration.GetConnectionString("Database"));
             });
 
-            services.AddIdentity<UserEntity, IdentityRole>(options =>
-                {
-                    options.User.RequireUniqueEmail = true;
-                })
-                .AddEntityFrameworkStores<StorageContext>()
-                .AddDefaultTokenProviders();
+            services.ConfigureIdentity();
+
             services.AddLogging(x => x.AddConsole());
             services.AddScoped<IFileRepository, FileRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
@@ -71,37 +61,12 @@ namespace FileStorage.WebApi
             var pathOptions = Configuration.GetSection("PathOption");
             services.Configure<PathOptions>(pathOptions);
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Info
-                {
-                    Title = "FileStorage API",
-                    Version = "v1"
-                });
-
-                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
-                {
-                    Description = "Standard Authorization header using the Bearer scheme. Example: \"bearer {token}\"",
-                    In = "header",
-                    Name = "Authorization",
-                    Type = "apiKey"
-                });
-
-                c.DocumentFilter<SecurityRequirementsDocumentFilter>();
-            });
+            services.ConfigureAutoMapper();
+            services.ConfigureSwagger();
+            services.ConfigureAuthorization(Configuration);
+            services.ConfigureCors();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddCors();
-
-            var mappingConfig = new MapperConfiguration(mc =>
-            {
-                mc.AddProfile(new MappingProfile());
-            });
-
-            var mapper = mappingConfig.CreateMapper();
-            services.AddSingleton(mapper);
-
-            ConfigureAuthorization(services);
         }
 
         public static void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -117,42 +82,16 @@ namespace FileStorage.WebApi
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FileStorage API"));
 
-            app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            app.UseCors("CorsPolicy");
             app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseMvc();
-        }
-
-        private void ConfigureAuthorization(IServiceCollection services)
-        {
-            var jwtAuthenticationSection = Configuration.GetSection("JwtAuthentication");
-            services.Configure<JwtAuthenticationOptions>(jwtAuthenticationSection);
-            var jwtAuthenticationOptions = jwtAuthenticationSection.Get<JwtAuthenticationOptions>();
-
-            services.AddAuthentication(x =>
-                {
-                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(x =>
-                {
-                    x.RequireHttpsMetadata = false;
-                    x.SaveToken = true;
-                    x.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = jwtAuthenticationOptions.SymmetricSecurityKey,
-                        ValidateIssuer = false,
-                        ValidateAudience = false
-                    };
-                });
         }
 
         private static void InitializeDatabase(IApplicationBuilder app)
